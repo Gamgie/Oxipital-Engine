9894281a768a0fbb24cb55eb2a7c6cc6caf32444
Gamgie <gamgie@pm.me>
2024-10-07 00:25:41 +0200

    add speed weight to circle and line

--- Unity/Assets/Scripts/Ballet/Patterns/LineDancePattern.cs
+++ Unity/Assets/Scripts/Ballet/Patterns/LineDancePattern.cs
@@ -4,45 +4,44 @@ using UnityEngine;
 
 namespace Oxipital
 {
-    public class LineDancePattern : DancePattern
-    {
-        override protected List<Vector3> getPatternPositions<T>(DancerGroup<T> group)
-        {
-            List<Vector3> positions = new List<Vector3>();
+	public class LineDancePattern : DancePattern
+	{
+		override protected List<Vector3> getPatternPositions<T>(DancerGroup<T> group)
+		{
+			List<Vector3> positions = new List<Vector3>();
 
-            if (group.items.Count <= 1)
-            {
-                positions.Add(Vector3.zero);
-                return positions;
-            }
+			if (group.items.Count <= 1)
+			{
+				positions.Add(Vector3.zero);
+				return positions;
+			}
 
-            int fullDancerCount = group.getFullDancersCount(); //count of full items
-            int dancerCount = group.items.Count; //count of all items, potential one more than full if count is not a whole number
-            float relativeProgression = group.getCountRelativeProgression(); //progression between full items
+			int fullDancerCount = group.getFullDancersCount(); //count of full items
+			int dancerCount = group.items.Count; //count of all items, potential one more than full if count is not a whole number
+			float relativeProgression = group.getCountRelativeProgression(); //progression between full items
 
-            for (int i = 0; i < dancerCount; i++)
-            {
-                float tAtFull = fullDancerCount == 1 ? 0 : (float)i / (fullDancerCount - 1);
-                float tAtNextFull = (float)(i) / (dancerCount - 1);
+			for (int i = 0; i < dancerCount; i++)
+			{
+				float tAtFull = fullDancerCount == 1 ? 0 : (float)i / (fullDancerCount - 1);
+				float tAtNextFull = (float)(i) / (dancerCount - 1);
 
-                float rel = i / group.count;
-                float initT = Mathf.Lerp(tAtFull, tAtNextFull, relativeProgression) + group.patternTimeOffset * rel;
-                float normalT = initT + group.patternTime;
-                float randomT = initT + group.items[i].localPatternTime;
+				float rel = i / group.count;
+				float initT = Mathf.Lerp(tAtFull, tAtNextFull, relativeProgression) + group.patternTimeOffset * rel;
+				float normalT = initT + group.patternTime;
+				float randomT = initT + group.items[i].localPatternTime;
 
+				Vector3 zeroFullPos = fullDancerCount == 1 ? Vector3.zero : Vector3.Lerp(Vector3.down, Vector3.up, tAtFull);
+				Vector3 zeroNextPos = Vector3.Lerp(Vector3.down, Vector3.up, tAtNextFull);
+				Vector3 zeroPos = Vector3.Lerp(zeroFullPos, zeroNextPos, relativeProgression);
 
-                Vector3 zeroFullPos = fullDancerCount == 1 ? Vector3.zero : Vector3.Lerp(Vector3.down, Vector3.up, tAtFull);
-                Vector3 zeroNextPos = Vector3.Lerp(Vector3.down, Vector3.up, tAtNextFull);
-                Vector3 zeroPos = Vector3.Lerp(zeroFullPos, zeroNextPos, relativeProgression);
+				Vector3 normalPos = Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(normalT * Mathf.PI * 2) * .5f + .5f);
+				Vector3 randomPos = Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(randomT * Mathf.PI * 2) * .5f + .5f);
+				Vector3 oscPos = Vector3.Lerp(normalPos, randomPos, group.patternSpeedRandom);
+				Vector3 targetPos = Vector3.Lerp(zeroPos, oscPos, Mathf.Abs(group.patternSpeed * speedWeight));
+				positions.Add(targetPos);
+			}
 
-                Vector3 normalPos = Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(normalT * Mathf.PI) * .5f + .5f);
-                Vector3 randomPos = Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(randomT * Mathf.PI) * .5f + .5f);
-                Vector3 oscPos = Vector3.Lerp(normalPos, randomPos, group.patternSpeedRandom);
-                Vector3 targetPos = Vector3.Lerp(zeroPos, oscPos, Mathf.Abs(group.patternSpeed));
-                positions.Add(targetPos);
-            }
-
-            return positions;
-        }
-    }
+			return positions;
+		}
+	}
 }
