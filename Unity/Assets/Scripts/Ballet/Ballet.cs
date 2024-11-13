722032d34bc40f6a3b24588a2b01c609d3babbb3
Gamgie <gamgie@pm.me>
2024-11-13 17:12:52 +0100

    Add comment to ballet

--- Unity/Assets/Scripts/Ballet/Ballet.cs
+++ Unity/Assets/Scripts/Ballet/Ballet.cs
@@ -19,12 +19,14 @@ namespace Oxipital
         // Update is called once per frame
         void Update()
         {
+            // Gather all force buffer
 			Dictionary<string, GraphicsBuffer> forceBuffers = new Dictionary<string, GraphicsBuffer>(standardForceManager.items.Count);
 			foreach (StandardForceGroup group in standardForceManager.items)
 			{
 				forceBuffers.Add(group.gameObject.name, group.buffer);
 			}
 
+            // Send it to orbs and compute total particle count
             totalParticles = 0;
             foreach (OrbGroup group in orbManager.items)
             {
