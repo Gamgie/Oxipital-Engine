add041e97e0b57c828c8dc38df84e169ae3f67e5
Gamgie <gamgie@pm.me>
2025-03-24 09:30:32 +0100

    add more positions in manual dance pattern

--- Unity/Assets/Scripts/Ballet/Patterns/ManualDancePattern.cs
+++ Unity/Assets/Scripts/Ballet/Patterns/ManualDancePattern.cs
@@ -12,6 +12,16 @@ namespace Oxipital
 		public Vector3 pos4;
 		public Vector3 pos5;
 		public Vector3 pos6;
+		public Vector3 pos7;
+		public Vector3 pos8;
+		public Vector3 pos9;
+		public Vector3 pos10;
+		public Vector3 pos11;
+		public Vector3 pos12;
+		public Vector3 pos13;
+		public Vector3 pos14;
+		public Vector3 pos15;
+		public Vector3 pos16;
 
 		override protected List<Vector3> getPatternPositions<T>(DancerGroup<T> group)
 		{
@@ -23,6 +33,16 @@ namespace Oxipital
 			positions.Add(pos4);
 			positions.Add(pos5);
 			positions.Add(pos6);
+			positions.Add(pos7);
+			positions.Add(pos8);
+			positions.Add(pos9);
+			positions.Add(pos10);
+			positions.Add(pos11);
+			positions.Add(pos12);
+			positions.Add(pos13);
+			positions.Add(pos14);
+			positions.Add(pos15);
+			positions.Add(pos16);
 
 			return positions;
 		}
