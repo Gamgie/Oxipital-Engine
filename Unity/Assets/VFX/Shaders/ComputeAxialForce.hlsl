965ea6a9abe2c0cff952d4b36b5578ff1a6b1bba
Gamgie <gamgie@pm.me>
2025-09-11 12:22:49 +0200

    add random per particle input

--- Unity/Assets/VFX/Shaders/ComputeAxialForce.hlsl
+++ Unity/Assets/VFX/Shaders/ComputeAxialForce.hlsl
@@ -1,10 +1,10 @@
 #ifndef COMPUTE_AXIAL_FORCE_H
 #define COMPUTE_AXIAL_FORCE_H
 
-#include "VFXCommon.hlsl"
+#include "VFXHelper.hlsl"
 
 
-float3 ComputeAxialForce(in float3 pos, in float3 rotation, in float normalizedDistance, in float3 center, in float3 axialFrequency, in float axialFactor, in float3 axisMultiplier)
+float3 ComputeAxialForce(in float3 pos, in float3 worldPosition, in float3 rotation, in float normalizedDistance, in float3 center, in float3 axialFrequency, in float axialFactor, in float3 axisMultiplier)
 {
     // Axial force is attracting towards axis
     // F = Intensity / pow(( R + 1 ),axialFactor)
@@ -18,55 +18,59 @@ float3 ComputeAxialForce(in float3 pos, in float3 rotation, in float normalizedD
     if (axisMultiplier.x != 0)
     {	
         float3 xAxis = ComputeRightVectorFromRotation(rotation);
-        float3 axialHorizontalVector = ClosestPointOnALine(pos, xAxis*axisMultiplier.x, center);
+        float3 axialHorizontalVector = ClosestPointOnALine(worldPosition, xAxis * axisMultiplier.x, center);
+        float axialXDistance = length(axialHorizontalVector);
 		float waveX = 0;
 
 		if(axialFrequency.x != 0)
         {
-			waveX = sin(2*PI*axialFrequency.x*pos.y) * cos(PI*axialFrequency.x*pos.x);
-		}
+			//waveX = sin(2*PI*axialFrequency.x*pos.y) * cos(PI*axialFrequency.x*pos.x);
+            waveX = sin(2 * PI * axialFrequency.x * pos.x);
+        }
 		else
         {
 			waveX = 1;
 		}
 
-        axialForceX = (axisMultiplier.x / pow(abs(normalizedDistance + 1), abs(axialFactor))) * waveX * normalize(axialHorizontalVector);
+        axialForceX = (axisMultiplier.x / pow(axialXDistance + 1, abs(axialFactor))) * waveX * normalize(axialHorizontalVector);
     }
 		
 	// Compute Y Axis
 	if(axisMultiplier.y != 0)
     {
 		float3 yAxis = ComputeUpVectorFromRotation(rotation);
-        float3 axialVerticalVector = ClosestPointOnALine(pos, yAxis * axisMultiplier.y, center);
+        float3 axialVerticalVector = ClosestPointOnALine(worldPosition, yAxis * axisMultiplier.y, center);
 		float waveY = 0;
+        float axialYDistance = length(axialVerticalVector);
 
 		if(axialFrequency.y != 0)
         {
-			waveY = sin(2*PI*axialFrequency.y*pos.x) * cos(PI*axialFrequency.y*pos.y);
-		}
+            waveY = sin(2 * PI * axialFrequency.y * pos.y);
+        }
 		else
         {
 			waveY = 1;
 		}
 
-        axialForceY = (axisMultiplier.y / pow(abs(normalizedDistance + 1), abs(axialFactor))) * waveY * normalize(axialVerticalVector);
+        axialForceY = (axisMultiplier.y / pow(axialYDistance + 1, abs(axialFactor))) * waveY * normalize(axialVerticalVector);
     }
 	// Compute Z Axis
     if (axisMultiplier.z != 0)
     {
 		float3 zAxis = ComputeForwardVectorFromRotation(rotation);
-        float3 axialDepthVector = ClosestPointOnALine(pos, zAxis*axisMultiplier.z, center);
+        float3 axialDepthVector = ClosestPointOnALine(worldPosition, zAxis * axisMultiplier.z, center);
+        float axialZDistance = length(axialDepthVector);
 		float waveZ = 0;
 
 		if(axialFrequency.z != 0)
         {
-			waveZ = sin(2*PI*axialFrequency.z*pos.z) * cos(PI*axialFrequency.z*pos.y);
-		}
+            waveZ = sin(2 * PI * axialFrequency.z * pos.z);
+        }
 		else
         {
 			waveZ = 1;
 		}
-        axialForceZ = (axisMultiplier.z / pow(abs(normalizedDistance + 1), abs(axialFactor))) * waveZ * normalize(axialDepthVector);
+        axialForceZ = (axisMultiplier.z / pow(axialZDistance + 1, abs(axialFactor))) * waveZ * normalize(axialDepthVector);
     }		
 
     // Total force contribution from this center
