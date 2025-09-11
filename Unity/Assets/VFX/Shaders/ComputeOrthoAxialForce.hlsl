8e1a133c03ccc0e11010644ffa00f500d2ae5bc6
Gamgie <gamgie@pm.me>
2025-09-11 13:54:11 +0200

    clean orthoaxial file

--- Unity/Assets/VFX/Shaders/ComputeOrthoAxialForce.hlsl
+++ Unity/Assets/VFX/Shaders/ComputeOrthoAxialForce.hlsl
@@ -9,19 +9,16 @@ float3 ComputeOrthoAxialForce(in float3 pos, in float3 rotation, in float3 cente
     // F = Intensity / pow(( R + 1 ),axialFactor)
 
 	float3 orthoAxialForce = float3(0.0,0.0,0.0);	
-    float rrr = 1;
     float3 zAxis = ComputeForwardVectorFromRotation(rotation);
     float3 xAxis = ComputeRightVectorFromRotation(rotation);
     float3 axialDepthVector = ClosestPointOnALine(pos, zAxis, center);
-    float3 orthoAxialDepthVector = cross(normalize(zAxis), normalize(axialDepthVector)) * clockwise;
+    float3 orthoAxialDepthVector = cross(normalize(zAxis), normalize(axialDepthVector)) * clockwise * -1;
     float axialZDistance = length(axialDepthVector) / normalizedRadius;
     float axialXDistance = length(ClosestPointOnALine(pos, xAxis, center)) / normalizedRadius;
     float curve = (axialXDistance);//+ sin(2 * 3.14 * axialXDistance);
 
     orthoAxialForce =  (1 / pow(axialZDistance + 1, abs(axialFactor))) * normalize(orthoAxialDepthVector);
 	
-    
-
     // Total force contribution from this center
     return orthoAxialForce;
 }
