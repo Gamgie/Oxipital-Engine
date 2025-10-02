5c84a428b3ef41bd2e16944b2e79fcd7ec7578e0
Gamgie <gamgie@pm.me>
2025-10-02 08:38:49 +0200

    update spaceship range and camera transform script

--- Unity/Assets/Scripts/Camera/CameraTransform.cs
+++ Unity/Assets/Scripts/Camera/CameraTransform.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
 
-public class ObjectPosition : MonoBehaviour
+public class CameraTransform : MonoBehaviour
 {
     public Transform targetObject;
     [Range(0f, 5f)]
     public float offset;
     public Vector3 position;
+    public Vector3 rotation;
 
     // Update is called once per frame
     void Update()
@@ -14,6 +15,7 @@ public class ObjectPosition : MonoBehaviour
         {
             targetObject.localPosition = new Vector3(0, 0, offset);
             position = targetObject.transform.position;
+            rotation = targetObject.transform.rotation.eulerAngles;
         }
     }
 }
