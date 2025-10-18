5e1fe2278cc94138c39a5d9ec7d6260ea2a414ea
Gamgie <gamgie@pm.me>
2025-10-19 01:12:25 +0200

    line dance pattern can oscillate in 2 direction

--- Unity/Assets/Scripts/Ballet/Patterns/LineDancePattern.cs
+++ Unity/Assets/Scripts/Ballet/Patterns/LineDancePattern.cs
@@ -6,6 +6,11 @@ namespace Oxipital
 {
 	public class LineDancePattern : DancePattern
 	{
+		[Range(0,1)]
+		public float oscilatingDirection = 0;
+		[Range(0, 1)]
+		public float oscilatingSize = 1;
+
 		override protected List<Vector3> getPatternPositions<T>(DancerGroup<T> group)
 		{
 			List<Vector3> positions = new List<Vector3>();
@@ -31,17 +36,14 @@ namespace Oxipital
 				float normalT = group.patternTime;
 				float randomT = group.items[i].localPatternTime;
 
-				//Vector3 zeroFullPos = fullDancerCount == 1 ? Vector3.zero : Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(Mathf.PI*tAtFull));
-				//Vector3 zeroNextPos = Vector3.Lerp(Vector3.down, Vector3.up, tAtNextFull);
-				//Vector3 zeroPos = Vector3.Lerp(zeroFullPos, zeroNextPos, relativeProgression);
-
-				//Vector3 normalPos = Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(normalT * Mathf.PI * 2) * .5f + .5f);
-				//Vector3 randomPos = Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(randomT * Mathf.PI * 2) * .5f + .5f);
-				//Vector3 oscPos = Vector3.Lerp(normalPos, randomPos, group.patternSpeedRandom);
-				//Vector3 targetPos = Vector3.Lerp(zeroPos, oscPos, Mathf.Abs(group.patternSpeed * speedWeight));
+				// We compute the line and each element position
 				Vector3 oscPos = Vector3.Lerp(Vector3.left, Vector3.right, Mathf.Cos(2*Mathf.PI*normalT + rel) * .5f + .5f);
-                Vector3 randomOscPos = Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(2 * Mathf.PI * randomT + rel) * .5f + .5f);
-                Vector3 targetPos = Vector3.Lerp(oscPos, randomOscPos, group.patternSpeedRandom); ;
+				Vector3 randomOscPos = Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(2 * Mathf.PI * randomT + rel) * .5f + .5f);
+
+				Vector3 linePos = Vector3.Lerp(Vector3.left, Vector3.right, Mathf.Cos(2 * Mathf.PI + rel) * .5f + .5f);
+				Vector3 orthoOscPos = Vector3.Lerp(Vector3.down, Vector3.up, Mathf.Cos(2 * Mathf.PI * normalT + rel) * .5f + .5f) * oscilatingSize;
+                Vector3 targetPos = Vector3.Lerp(oscPos, randomOscPos, group.patternSpeedRandom);
+				targetPos = Vector3.Lerp(targetPos, linePos + orthoOscPos, oscilatingDirection);
 
                 positions.Add(targetPos); 
 			}
