81751f2f527c451367c8fdfd785f3dd42110619c
Gamgie <gamgie@pm.me>
2026-09-18 17:43:29 +0200

    deactivate spout when in dome mode

--- Unity Oxipital/Oxipital-Engine/Assets/Scripts/SpoutFeedback.cs
+++ Unity Oxipital/Oxipital-Engine/Assets/Scripts/SpoutFeedback.cs
@@ -6,8 +6,25 @@ public class SpoutFeedback : MonoBehaviour
 {
     public SpoutSender spout;
 
+    CameraController cameraController;
+
+    public void OnEnable()
+    {
+         if (!cameraController)
+            cameraController = FindFirstObjectByType<CameraController>();
+        if (cameraController != null && cameraController.isFullDome)
+        {
+            transform.parent.gameObject.SetActive(false);
+        }
+
+        return;
+    }
+
     void Update()
     {
+       if (cameraController != null && cameraController.isFullDome)
+            return;
+
         GetComponent<RawImage>().texture = spout.sourceTexture;
     }
 
